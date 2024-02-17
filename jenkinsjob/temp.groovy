acfac0c7bc899cffe38c16fae8a5104ef3e986a0
root <root@ip-172-31-12-209.ap-northeast-1.compute.internal>
2024-02-17 09:40:03 +0000

    ok

---
+++ jenkinsjob/temp.groovy
@@ -0,0 +1,14 @@
+pipelineJob('temp') {
+    displayName('display name of temp groovy file')
+
+    parameters {
+        stringParam(parameterName="name", defaultValue="Aniket", description="your name")
+    }
+
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelines/temp.Jenkinsfile'))
+            sandbox()
+        }
+    }
+}
